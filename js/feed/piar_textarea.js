26f60dc6ba583a9d3518312d2be670dc1419f6b7
Henrique <henrique@falconer.com.br>
2020-04-12 00:39:30 -0300

    added content to feed page

---
+++ js/feed/piar_textarea.js
@@ -0,0 +1,28 @@
+// ---------------------------------------------------
+// CONTROLAR FUNCIONALIDADES DA CAIXA DE TEXTO DE PIAR
+// ---------------------------------------------------
+
+autoSizeTextArea();
+
+function autoSizeTextArea() {
+    var textArea = document.querySelector("#piar_textfield");
+    textArea.addEventListener("input", function() {
+
+        // Reset field height
+        this.style.height = 'inherit';
+    
+        // Get the computed styles for the element
+        var computed = window.getComputedStyle(this);
+    
+        // Calculate the height
+        var height = parseInt(computed.getPropertyValue('border-top-width'), 10)
+                     + parseInt(computed.getPropertyValue('padding-top'), 10)
+                     + this.scrollHeight
+                     + parseInt(computed.getPropertyValue('padding-bottom'), 10)
+                     + parseInt(computed.getPropertyValue('border-bottom-width'), 10);
+    
+        console.log(height);
+        this.style.height = height + 'px';
+    });
+}
+
